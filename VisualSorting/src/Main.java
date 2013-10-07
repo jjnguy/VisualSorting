b8a0a1cf4dcf9c46eb40da86423c59a54ee991b3
Justin Nelson <justin.john.nelson@gmail.com>
2013-10-06 22:56:23 -0500

    cleaned up main method

--- VisualSorting/src/Main.java
+++ VisualSorting/src/Main.java
@@ -21,21 +21,17 @@ public class Main {
 
 	/**
 	 * @param args
-	 * @throws InterruptedException 
+	 * @throws InterruptedException
 	 */
 	public static void main(String[] args) throws InterruptedException {
 		Sorter s = new SmarterQuickSorter(SorterTester.createAlmostSortedArray(1200, 10), 1);
 		final SortFrame frame = new SortFrame();
 		frame.resetUsingConfig();
-		// frame.addSorter(s);
-		//s.sort();
-		//System.out.println("Time: " + s.time());
-		
-		/*
+/*
 		long totalDumb = 0;
 		long totalSmart = 0;
-		for (int i = 0; i < 10000; i++) {
-			int[] test = SorterTester.createRandomArray(10000);
+		for (int i = 0; i < 1000; i++) {
+			int[] test = SorterTester.createAlmostSortedArray(100000, 10);
 			Sorter dumb = new SimpleQuickSorter(Arrays.copyOf(test, test.length), 0);
 			Sorter smart = new SmarterQuickSorter(Arrays.copyOf(test, test.length), 0);
 			smart.sort();
@@ -45,19 +41,6 @@ public class Main {
 		}
 		System.out.println("Total smart: " + totalSmart);
 		System.out.println("Total dumbb: " + totalDumb);
-		
-		int[] toTest = SorterTester.createRandomArray(100000);
-		SorterTester test = new BubbleSorterTester();
-		test.test(toTest);
-		SorterTester test5 = new InsertionSorterTester();
-		test5.test(toTest);
-		SorterTester test2 = new SlowMergeSorterTester();
-		test2.test(toTest);
-		SorterTester test3 = new QuickSorterTest();
-		test3.test(toTest);
-		SorterTester test4 = new BuiltInSorterTester();
-		test4.test(toTest);
-		
 		*/
 	}
 }
